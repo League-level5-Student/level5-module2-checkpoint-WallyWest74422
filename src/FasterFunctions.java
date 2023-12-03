ad566d0224901a53a5536e386c5e4d6bf89f1758
jereycui2008 <jeremycuid@gmail.com>
2023-12-03 11:32:21 -0800

    wefgawegreg

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,15 +1,56 @@
-
+//1. Go to Run > Run Configurations
+// *      2. Click on the tab that reads "(x)= Arguments"
+// *      3. In the box labeled "VM arguments:", add this: -Xss64m 
+// * 
 public class FasterFunctions {
 	public static void main(String[] args) {
-		float totalTime = 0;
+	float totalTime = 0;
 		long startTime = System.currentTimeMillis();
+
 		//Your Code Here
-		
-		
-		
-		
+		Thread uno = new Thread(() -> {
+			SlowFunctions.slowSortLargeArray();
+			System.out.println("slowSortLargeArray() Complete.");
+		});
+		Thread dos = new Thread(() -> {
+			System.out.println("ackermann(3, 14): " + SlowFunctions.ackermann(3, 14));
+			System.out.println("ackermann(3, 14) Complete.");		
+		});
+		Thread tres = new Thread(() -> {
+			SlowFunctions.millionsOfSqrts();
+			System.out.println("millionsOfSqrts() Complete.");
+		});
+		try {
+			uno.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			dos.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			tres.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		uno.start();
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
 		totalTime += endTime;
-		System.out.println("Total Time: " + totalTime);
+		System.out.println("slowSortLargeArray() Time: " + endTime + "\n\n");
+		startTime = System.currentTimeMillis();
+		dos.start();
+		endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+		totalTime += endTime;
+		System.out.println("ackerman(3, 14) Time: " + endTime + "\n\n");
+		tres.start();
+		endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+		totalTime += endTime;
+		
+		System.out.println("Total Time: " + totalTime + "\n\n");
 	}
 }
